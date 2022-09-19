4a46a61e6c7ba1728ec4306554589665d7322798
Murilo <murilodesousag@gmail.com>
2022-09-19 17:55:32 -0300

    explicar semana 3

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
+
+
 const pizzaMussarela = {
     nome: "pizza mussarela",
     preço: 10,
-    disponivel: true
-}
-const pizzaPalmito = {
-    nome: "pizza de palmito",
-    preço: 12,
-    disponivel: true
+    disponivel: true,
+    ingredientes: ["farinha", "queijo", "tomate"]
 }
 
-const comidas = []
 
+//Semana 3
+//Ex1 - loop para traforar array em string
 
-if (pizzaMussarela.disponivel){
-    comidas.push(pizzaMussarela)
+let ingredientesString= ""
+
+for(let i=0;i<pizzaMussarela.ingredientes.length; i++){
+    ingredientesString += pizzaMussarela.ingredientes[i] + ", "
 }
-comidas.push(pizzaMussarela, pizzaPalmito)
+
+console.log(ingredientesString)
+
+pizzaMussarela.ingredientes = ingredientesString
+
+console.log(`RELATORIO:
+${pizzaMussarela.nome}
+${pizzaMussarela.preço}
+`)
 
 
-for(elemento in elementos){
-    console.log(elemento.item);
-  }
+//Ex 3 - função para tranasforma objeto em string
+
+function mudarPraString(comida){
+    for(i in comida){
+        comidaString += comida[i] + ", "
+    }
+    return comidaString
+}
+
+
+
